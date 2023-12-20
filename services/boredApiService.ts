b4587ab9f78adc5553525918e16b2854ddbcecad
Dagaet <alojera15@gmail.com>
2023-12-20 10:15:52 +0000

    feat(boredApiService): added api connection and json response

---
+++ services/boredApiService.ts
@@ -0,0 +1,27 @@
+const BORED_API_URL = "https://www.boredapi.com";
+const ACTIVITIES_PATH = "/api/activity";
+
+type BoredActivityResponse = {
+  activity: string;
+};
+
+const getInitRequest = (httpVerb: string) => {
+  const init: RequestInit = {
+    method: httpVerb,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
+  return init;
+};
+
+export const getActivity = async (): Promise<string> => {
+  const response = await fetch(BORED_API_URL, getInitRequest("GET"));
+  const json: BoredActivityResponse = await response.json();
+
+  if (json != null) {
+    return json.activity;
+  }
+  return "Error";
+};
