7871e3bb7efa01a4d8dbcfd7b4e26ae5db6becf9
Dagaet <alojera15@gmail.com>
2024-01-09 09:52:24 +0000

    feat(RegisterScreen): created register screen

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,14 +1,90 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import appColors from "../assets/Styles/appColors";
+import { NavigationContext } from "@react-navigation/native";
 
 const RegisterScreen = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = React.useContext(NavigationContext);
+
+  function register() {
+    console.log("Register");
+  }
+
   return (
-    <View>
-      <Text>RegisterScreen</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.loginContainer}>
+        <TextInput
+          placeholder="Name"
+          style={styles.input}
+          value={name}
+          onChangeText={setName}
+        ></TextInput>
+        <TextInput
+          placeholder="Email"
+          style={styles.input}
+          value={name}
+          onChangeText={setName}
+        ></TextInput>
+        <TextInput
+          placeholder="Password"
+          style={styles.input}
+          value={password}
+          secureTextEntry={true}
+          onChangeText={setPassword}
+        ></TextInput>
+        <Pressable style={styles.logInButton} onPress={register}>
+          <Text style={styles.logInButtonText}>Click to register</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
 
 export default RegisterScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    paddingTop: 120,
+    alignItems: "center",
+    backgroundColor: appColors.secondary,
+  },
+  loginContainer: {
+    backgroundColor: appColors.grey,
+    width: 350,
+    height: 370,
+    borderTopLeftRadius: 50,
+    borderBottomRightRadius: 50,
+    paddingTop: 30,
+  },
+  loginSubContainer: {
+    justifyContent: "space-evenly",
+  },
+  logInButton: {
+    marginTop: 40,
+    borderRadius: 50,
+    backgroundColor: appColors.buttonColor,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    width: 150,
+  },
+  input: {
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    backgroundColor: appColors.secondary,
+    borderRadius: 50,
+    padding: 10,
+    height: 40,
+    width: 230,
+  },
+  logInButtonText: {
+    fontSize: 20,
+    fontWeight: "700",
+  },
+});
